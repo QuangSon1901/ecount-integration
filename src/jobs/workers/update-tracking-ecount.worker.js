da1b45c7ff11fd69b6091d1eb6196ecc2a25826e
luutruso01 <luutruso01@luutruso01s-MacBook-Pro.local>
2025-11-20 13:58:11 +0700

    SDF

--- src/jobs/workers/update-tracking-ecount.worker.js
+++ src/jobs/workers/update-tracking-ecount.worker.js
@@ -9,7 +9,7 @@ class UpdateTrackingEcountWorker extends BaseWorker {
     constructor() {
         super('update_tracking_ecount', {
             intervalMs: 5000,
-            concurrency: 2  // Chạy đồng thời 1 jobs (vì có Puppeteer nặng)
+            concurrency: 1  // Chạy đồng thời 1 jobs (vì có Puppeteer nặng)
         });
     }
 
