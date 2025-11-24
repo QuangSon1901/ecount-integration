2d8441ce8c4cbd39102c87516926031410a241e7
Vũ Quang Sơn <93460718+QuangSon1901@users.noreply.github.com>
2025-11-24 15:16:00 +0700

    ád

--- src/config/carriers.config.js
+++ src/config/carriers.config.js
@@ -4,13 +4,13 @@ module.exports = {
         name: 'YunExpress',
         code: 'YUNEXPRESS',
         enabled: true,
-        productCodes: ['YTYCPREC', 'VN-YTYCPREC', 'VNTHZXR', 'VNMUZXR'] // Các mã sản phẩm có sẵn
+        productCodes: ['VN-YTYCPREC', 'VNTHZXR', 'VNBKZXR', 'VNMUZXR'] // Các mã sản phẩm có sẵn
     },
     YUNEXPRESS_CN: {
         name: 'YunExpress China',
         code: 'YUNEXPRESS_CN',
         enabled: true,
-        productCodes: ['ZBZXRPH', 'YTYCPREG', 'YTYCPREC', 'FZZXR', 'BKPHR', 'THPHR', 'THZXR', 'BKZXR', 'MUZXR'] // Các mã sản phẩm có sẵn
+        productCodes: ['YTYCPREG', 'YTYCPREC', 'FZZXR', 'BKPHR', 'THPHR', 'THZXR', 'BKZXR', 'MUZXR', 'ZBZXRPH'] // Các mã sản phẩm có sẵn
     },
     // DHL: {
     //     name: 'DHL',
